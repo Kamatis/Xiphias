4f3dfe64f3ec79b02d0caa3d3328d055e13d6a78
Kamatis <rosales.matthew.allan@gmail.com>
2015-07-09 07:19:10 +0800

    changed image src to base_url

--- application/views/dashboard/questRarity.php
+++ application/views/dashboard/questRarity.php
@@ -1,7 +1,7 @@
 <div class="col-md-5ths">
   <div class="choice" data-toggle="radio">
     <input type="radio" name="rarity" value="<?php echo $rarity_id; ?>" id="rarity<?php echo $rarity_id; ?>" data-minExp="<?php echo $rarity_range_min; ?>" data-maxExp="<?php echo $rarity_range_max; ?>">
-    <img class="icon" src="<?php echo $rarity_icon; ?>">
+    <img class="icon" src="<?php echo base_url($rarity_icon); ?>">
     <h6><?php echo $rarity_name; ?></h6>
   </div>
 </div>
