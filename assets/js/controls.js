2e928bd500472c96023c83019711c60f8666a216
Kamatis <rosales.matthew.allan@gmail.com>
2015-07-09 07:02:03 +0800

    Deleted past date templates for quest duration

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -258,11 +258,7 @@ $('input[name="date-range"]').daterangepicker({
         timePicker12Hour: true,
         ranges: {
            'Today': [moment(), moment()],
-           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
            'This Month': [moment().startOf('month'), moment().endOf('month')],
-           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         },
         opens: 'left',
         drops: 'down',
