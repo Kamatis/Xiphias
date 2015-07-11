cc3eaa8167cb253be8bb63198fbfc6e0a9e78f00
kellymilla18 <kellymilla18@gmail.com>
2015-07-11 10:59:26 +0800

    transfered getBadgeInfo to ajax controller

---
+++ application/controllers/ajax.php
@@ -0,0 +1,20 @@
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Ajax extends CI_Controller {
+
+    public function getBadgeDetails() {
+        $badge_id = $this->input->post('badge_id');
+        $badge = $this->badge->getBadgeInfo($badge_id); 
+        $badgelvls = $this->badge->getBadgeUpgrades($badge_id);
+        for($x = 0; $x < count($badgelvls); $x++) 
+            $badge['upgradeViews'] .= $this->load->view('dashboard/badgeUpgrade.php', $badgelvls[$x], true);
+        $json = array(
+              'baseLvlBadge'  => base_url($badge['baseLvlBadge']),
+              'name'          => $badge['name'],
+              'description'   => $badge['description'],
+              'badgeLvls'     => $badge['upgradeViews']
+        );
+        echo json_encode($json);
+    }
+
+}
--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -180,21 +180,6 @@ class Pages extends CI_Controller {
     
     $this->load->view('dashboard/badgeUpgrade.php', $badge);
   }
-  
-    public function getBadgeDetails() {
-        $badge_id = $this->input->post('badge_id');
-        $badge = $this->badge->getBadgeInfo($badge_id); 
-        $badgelvls = $this->badge->getBadgeUpgrades($badge_id);
-        for($x = 0; $x < count($badgelvls); $x++) 
-            $badge['upgradeViews'] .= $this->load->view('dashboard/badgeUpgrade.php', $badgelvls[$x], true);
-        $json = array(
-              'baseLvlBadge'  => base_url($badge['baseLvlBadge']),
-              'name'          => $badge['name'],
-              'description'   => $badge['description'],
-              'badgeLvls'     => $badge['upgradeViews']
-        );
-        echo json_encode($json);
-    }
     
     public function getQuestDetails() {
         $questId = $this->input->post('quest_id');
