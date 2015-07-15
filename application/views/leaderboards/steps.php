e3d9ca2980b66b2b8c41bdd07906fe25eb4c4db1
Kamatis <rosales.matthew.allan@gmail.com>
2015-07-15 16:20:02 -0700

    added leaderboards views

---
+++ application/views/leaderboards/steps.php
@@ -0,0 +1,41 @@
+<div class="stair-step house-<?php echo $houseid; ?>">
+  <div class="header-name house-<?php echo $houseid; ?>"><?php echo $playerName; ?></div>
+  <div class="header-lvl-house house-<?php echo $houseid; ?>">
+    <p>Lvl <?php echo $playerLevel; ?></p>
+    <img src="<?php echo $houseLogo; ?>" style="width: 50px; height: 50px;">
+  </div>
+  <div class="body-badge-award">
+    <img src="<?php echo $sembadge; ?>" class="place-second">
+  </div>
+  <div class="footer-place house-<?php echo $houseid; ?>">
+    2nd
+  </div>
+</div>
+
+<div class="stair-step house-<?php echo $houseid; ?>">
+  <div class="header-name house-<?php echo $houseid; ?>"><?php echo $playerName; ?></div>
+  <div class="header-lvl-house house-<?php echo $houseid; ?>">
+    <p>Lvl <?php echo $playerLevel; ?></p>
+    <img src="<?php echo $houseLogo; ?>" style="width: 50px; height: 50px;">
+  </div>
+  <div class="body-badge-award">
+    <img src="<?php echo $sembadge; ?>" class="place-first">
+  </div>
+  <div class="footer-place house-<?php echo $houseid; ?>">
+    1st
+  </div>
+</div>
+
+<div class="stair-step house-<?php echo $houseid; ?>">
+  <div class="header-name house-<?php echo $houseid; ?>"><?php echo $playerName; ?></div>
+  <div class="header-lvl-house house-<?php echo $houseid; ?>">
+    <p>Lvl <?php echo $playerLevel; ?></p>
+    <img src="<?php echo $houseLogo; ?>" style="width: 50px; height: 50px;">
+  </div>
+  <div class="body-badge-award">
+    <img src="<?php echo $sembadge; ?>" class="place-third">
+  </div>
+  <div class="footer-place house-<?php echo $houseid; ?>">
+    3rd
+  </div>
+</div>
