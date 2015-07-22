02004ee4a6936109a4f1b04c4049dcac11a9f370
kellymilla18 <kellymilla18@gmail.com>
2015-07-23 02:40:44 +0800

    added getHousePoints to pages controller

--- application/models/house.php
+++ application/models/house.php
@@ -19,4 +19,25 @@ class House extends CI_Model {
         $this->db->where('house_id', $house_id);
         return $this->db->get('house')->row()->house_name;
     }
+    
+    public function getHousePoints() {
+        $houses = $this->db->get('house');
+        $x = 0;
+        foreach($houses->result() as $house) {
+            $house_id = $house->house_id;
+            $map[$house_id] = $x;
+            $data[$x]['name'] = $house->house_name;
+            $data[$x]['y']    = 0;
+            $x++;
+        }
+        
+        $this->db->where('`date_completed` IS NOT NULL', null, false);
+        $completed = $this->db->get('quest_registration');
+        
+        foreach($completed->result() as $quest) {
+            $house_id = $this->user->getHouseId($quest->user_id);
+            $data[$map[$house_id]]['y'] += $this->quest->getHousePoints($quest->quest_id);
+        }
+        return $data;
+    }
 }
