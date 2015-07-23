ddef8b2e135e77a0835d8deb35d0aec69f1c984f
Kamatis <rosales.matthew.allan@gmail.com>
2015-07-23 12:48:30 +0800

    stylized rankings table & added placeholders for quest ads and ticker

--- application/views/index.php
+++ application/views/index.php
@@ -1,5 +1,13 @@
 <div class="wrapper">
-  <div class="container fill">
-    <?php echo $carousel ?>
+  <div class="col-md-9">
+    <div class="col-md-12">
+      <?php echo $carousel ?>
+    </div>
+    <div class="col-md-12" style="margin-top: 10px">
+      <img class="img-responsive" src="http://placehold.it/1200x150/16a085/ffffff?text=Quest Ads">
+    </div>
+  </div>
+  <div class="col-sm-3">
+      <img class="img-responsive" src="http://placehold.it/1200x2250/16a085/ffffff?text=Ticker">
   </div>
 </div>
--- assets/js/charts.js
+++ assets/js/charts.js
@@ -2,7 +2,8 @@ $(function () {
   
   var options = {
     chart: {
-      type: 'column'
+      type: 'column',
+      backgroundColor: "#16a085"
     },
     title: {
       text: 'HOUSE RANKING'
@@ -17,25 +18,26 @@ $(function () {
       title: {
         text:   'Houses',
         align:  'middle'
-      }
+      },
+      plotBands: [{
+        color: '#66cc88',
+        from: -1,
+        to: 0.5
+      }, {
+        color:  '#d9d96c',
+        from:   0.5,
+        to:     1.5
+      }, {
+        color:  '#cc6666',
+        from:   1.5,
+        to:     2.5
+      }, {
+        color:  '#6688cc',
+        from:   2.5,
+        to:     3.5
+      }],
     },
-    plotBands: [{
-      color: '#66cc88',
-      from: -1,
-      to: 0.5
-    }, {
-      color:  '#d9d96c',
-      from:   0.5,
-      to:     1.5
-    }, {
-      color:  '#cc6666',
-      from:   1.5,
-      to:     2.5
-    }, {
-      color:  '#6688cc',
-      from:   2.5,
-      to:     3.5
-    }],
+    
     yAxis: {
       title: {
         text: "House Points (HP)"
@@ -47,12 +49,15 @@ $(function () {
         dataLabels: {
           enabled: true,
           format: '{point.y}'
-        }
-      },
-      colors: ['#adedc2', '#ededb6', '#f2aaaa', '#a6beed']
+        },
+        colors: ['#adedc2', '#ededb6', '#f2aaaa', '#a6beed']
+      }
+      
     },
     series: [{
-      name: "Houses",
+      name: "House Points (HP)",
+      showInLegend: false,
+      type: 'column',
       colorByPoint: true
     }]
   };
@@ -61,5 +66,23 @@ $(function () {
     options.series[0].data = data;
     console.log(options);
     $('#house-ranks-chart').highcharts(options);
-  });   
+  });
+  
+  var clickEvent = false;
+  $('#myCarousel').on('click', '.nav a', function() {
+          clickEvent = true;
+          $('.nav li').removeClass('active');
+          $(this).parent().addClass('active');		
+  }).on('slid.bs.carousel', function(e) {
+      if(!clickEvent) {
+          var count = $('.nav').children().length -1;
+          var current = $('.nav li.active');
+          current.removeClass('active').next().addClass('active');
+          var id = parseInt(current.data('slide-to'));
+          if(count == id) {
+              $('.nav li').first().addClass('active');	
+          }
+      }
+      clickEvent = false;
+  });
 });
