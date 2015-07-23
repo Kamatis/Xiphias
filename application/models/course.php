0a7678f058b241394696d3c4bcd120525a73d01c
kellymilla18 <kellymilla18@gmail.com>
2015-07-23 21:31:34 +0800

    added get functions in course model

--- application/models/course.php
+++ application/models/course.php
@@ -1,6 +1,11 @@
 <?php
 
 class Course extends CI_Model{
+    public function getCourseName($program_id) {
+        $this->db->where('program_id', $program_id);
+        return $this->db->get('program')->row()->program_name;
+    }
+    
     public function getCourses(){
         return $this->db->get('program');   
     }
