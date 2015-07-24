4d0bddc11c72d4f14c42394ae6f3544c17abaae6
Kamatis <rosales.matthew.allan@gmail.com>
2015-07-24 16:39:20 +0800

    restructured columns

--- application/views/index.php
+++ application/views/index.php
@@ -1,13 +1,15 @@
 <div class="wrapper">
-  <div class="col-md-9">
+  <div class="col-sm-3 col-sm-push-9">
+    <div id="feeder">
+      
+    </div>
+  </div>
+  <div class="col-sm-9 col-sm-pull-3">
     <div class="col-md-12">
-      <?php echo $carousel ?>
+      <?php echo $carousel; ?>
     </div>
     <div class="col-md-12" style="margin-top: 10px">
       <img class="img-responsive" src="http://placehold.it/1200x150/16a085/ffffff?text=Quest Ads">
     </div>
-  </div>
-  <div class="col-sm-3">
-      <img class="img-responsive" src="http://placehold.it/1200x2250/16a085/ffffff?text=Ticker">
   </div>
 </div>
