42cf474b522afbe126c63a0fedd9c2b663a95f37
Kamatis <rosales.matthew.allan@gmail.com>
2015-07-28 10:21:33 +0800

    replaced client script

--- assets/socket/client.js
+++ assets/socket/client.js
@@ -1,8 +1,6 @@
 var socket = io.connect('http://localhost:8080');
 
 socket.on('feed', function(data){
-  var actualContent = $("#feeder").html();
-  var msgContent = '<li>' + data.name + '</li>';
-  var concatCOntent = msgContent + actualContent;
-  $('#feeder').html(concatCOntent);
+  console.log('client feed');
+  $('#feeder').prepend(data.streamItem);
 })
