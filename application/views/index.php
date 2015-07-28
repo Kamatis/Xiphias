d8b3167282738058ebc57d13493081b7566deac2
Kamatis <rosales.matthew.allan@gmail.com>
2015-07-28 10:23:13 +0800

    restructured view to get data from controller

--- application/views/index.php
+++ application/views/index.php
@@ -1,15 +1,25 @@
-<div class="wrapper">
+<!--credits-->
+<!--<a href='http://www.freepik.com/free-vector/variety-of-megaphones_789965.htm'>Designed by Freepik</a>-->
+
+<div class="container-fluid">
   <div class="col-sm-3 col-sm-push-9">
-    <div id="feeder">
-      
+      <div id="feeder-container">
+        <div class="banner">
+          <strong>Activity Stream</strong>
+        </div>
+        <div id="stream-container" >
+          <ul id="feeder">
+            <?php echo $stream; ?>
+          </ul>
+        </div>
     </div>
   </div>
   <div class="col-sm-9 col-sm-pull-3">
-    <div class="col-md-12">
+    <div >
       <?php echo $carousel; ?>
     </div>
-    <div class="col-md-12" style="margin-top: 10px">
-      <img class="img-responsive" src="http://placehold.it/1200x150/16a085/ffffff?text=Quest Ads">
+    <div class="" style="margin-top: 10px">
+      <img class="img-responsive" src="http://placehold.it/1200x450/16a085/ffffff?text=Quest Ads">
     </div>
   </div>
 </div>
