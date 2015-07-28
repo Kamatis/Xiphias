4d4332b5e49de4e1e047d864d134096b11a08e2a
Kamatis <rosales.matthew.allan@gmail.com>
2015-07-29 04:52:50 +0800

    added date and time to streamItem

--- application/views/index/streamItem.php
+++ application/views/index/streamItem.php
@@ -1,11 +1,12 @@
-<li style="list-style: none; padding: 10px; ">
+<li style="list-style: none; padding: 10px;">
   <div class="col-sm-10">
     <strong><a href="#"><?php echo $username; ?></a></strong> <?php echo $description; ?>
   </div>
   <div class="col-sm-2" style="padding: 0px !important;">
     <img src="http://img2.wikia.nocookie.net/__cb20120903181723/thehungergames/images/0/0a/Cute-pikachu-yellow-Favim.com-302682.jpg" alt="Pikachu" width="50" height="50">
   </div>
+  <div style="padding-left: 15px; font-size: 9pt; color: #868686"><small><?php echo $date; ?></small></div>
 </li>
-<div class="container-fluid">
-  <hr>
+<div class="container-fluid" style="padding: 0px;">
+  <hr style="margin: 0px;">
 </div>
