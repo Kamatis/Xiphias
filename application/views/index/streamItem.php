3f9aea33cd2563c3997fc0d7fa99bfeced5130f6
Kamatis <rosales.matthew.allan@gmail.com>
2015-08-04 09:55:03 +0800

    made img src dynamic

--- application/views/index/streamItem.php
+++ application/views/index/streamItem.php
@@ -2,8 +2,10 @@
   <div class="col-sm-10">
     <strong><a href="#"><?php echo $username; ?></a></strong> <?php echo $description; ?>
   </div>
-  <div class="col-sm-2" style="padding: 0px !important;">
-    <img src="http://img2.wikia.nocookie.net/__cb20120903181723/thehungergames/images/0/0a/Cute-pikachu-yellow-Favim.com-302682.jpg" alt="Pikachu" width="50" height="50">
+  <div class="col-sm-2" style="padding: 0px !important;" width="50" height="50">
+    <img src="<?php echo $pic_src; ?>" width="50" height="50"
+         <?php if ($pic_src == "") echo 'style="opacity: 0;"'; ?>
+         >
   </div>
   <div style="padding-left: 15px; font-size: 9pt; color: #868686"><small><?php echo $date; ?></small></div>
 </li>
