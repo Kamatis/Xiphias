4e95cce7a707df937b37d01b43c06a51762cf1e8
Kamatis <rosales.matthew.allan@gmail.com>
2015-08-12 06:11:24 +0800

    added hall of fame view

---
+++ application/views/famehall.php
@@ -0,0 +1,7 @@
+
+  <div class="ribbon">
+    <div class="ribbon-stitches-top"></div>
+    <strong class="ribbon-content"><h1><i class="fa fa-star"></i> Hall of Fame <i class="fa fa-star"></i></h1></strong>
+    <div class="ribbon-stitches-bottom"></div>
+  </div>
+
