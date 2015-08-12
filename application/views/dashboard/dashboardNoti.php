cab27eb19873fa780f0c39cf41d3525e06b34e20
Kamatis <rosales.matthew.allan@gmail.com>
2015-08-12 18:26:57 +0800

    added notification view

---
+++ application/views/dashboard/dashboardNoti.php
@@ -0,0 +1,18 @@
+<div id="dashboard-notifs" class="dashboard-noti">
+  <div class="table-container">
+    <table id="noti-table" data-toggle="table" data-show-header="false">
+      <thead>
+        <tr>
+          <th data-field="request" class="col-md-12">Request</th>
+          <th data-field="date" class="col-md-3">Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Request 1</td>
+          <td>September 8</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
