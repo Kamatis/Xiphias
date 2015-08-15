9c3b32c47d829b9ed10037f654f65fc78b8c4e30
Kamatis <rosales.matthew.allan@gmail.com>
2015-08-15 11:19:29 +0800

    made famehall view dynamic

--- application/views/famehall.php
+++ application/views/famehall.php
@@ -1,7 +1,11 @@
+<div class="ribbon">
+  <div class="ribbon-stitches-top"></div>
+  <strong class="ribbon-content"><h1><i class="fa fa-star"></i> Hall of Fame <i class="fa fa-star"></i></h1></strong>
+  <div class="ribbon-stitches-bottom"></div>
+</div>
 
-  <div class="ribbon">
-    <div class="ribbon-stitches-top"></div>
-    <strong class="ribbon-content"><h1><i class="fa fa-star"></i> Hall of Fame <i class="fa fa-star"></i></h1></strong>
-    <div class="ribbon-stitches-bottom"></div>
-  </div>
-
+<div style="margin-top: 100px;">
+  <section id="cd-timeline" class="cd-container">
+      <?php echo $fameitem; ?>
+  </section>
+</div>
