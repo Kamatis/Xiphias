4931d945b00747f6a5e8df2ecf9da65e7d07f713
Kamatis <rosales.matthew.allan@gmail.com>
2015-08-17 07:15:54 +0800

    added fb login, fb js initializatiion

--- assets/js/integration.js
+++ assets/js/integration.js
@@ -1,4 +1,31 @@
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if(d.getElementById(id)) { return; }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+} (document, 'script', 'facebook-jssdk'));
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId   : '979528042111341',
+    xfbml   : true,
+    cookie  : true,
+    version : 'v2.4'
+  });
+};
+
 $('#fb-link').on('click', function(e){
   e.preventDefault();
-  
+  FB.login(function(response){
+    if(response.authResponse) {
+      $.ajax({
+        url: 'FB_exchangeToken',
+        type: 'post'
+      });
+    }
+  }, {
+    scope: 'publish_actions'
+  });
 });
