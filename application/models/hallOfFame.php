990710cd2a20f50ce8b375091eef93e6602577ab
kellymilla18 <kellymilla18@gmail.com>
2015-08-17 09:38:51 +0800

    added hallOfFame model

---
+++ application/models/hallOfFame.php
@@ -0,0 +1,17 @@
+<?php
+    
+class HallOfFame extends CI_Model {
+    public function getHallOfFame() {
+        $hof = $this->db->get('hall_of_fame');
+        $x = $hof->num_rows() - 1;
+        foreach($hof->result() as $row) {
+            $ranking = $this->ranking->getHouseRankings($row->hof_id);
+            $data[$x]['date']         = $row->description;
+            $data[$x]['houseLogo1st'] = $this->house->getHouseLogo($ranking[0]['house_id']);
+            for($y = 0; $y < count($ranking); $y++)
+                $data[$x]['house'][$y] = $ranking[$y]['house_name'];
+            $x--;
+        }
+        return $data;
+    }
+}
