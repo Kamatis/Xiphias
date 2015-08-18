9011ad1b713e2bf5844879d316708d828817b536
Kamatis <rosales.matthew.allan@gmail.com>
2015-08-18 16:36:43 +0800

    added form data attributes / deleted add button for serial & noti / added count label for noti

--- application/views/dashboard/dashboardMenu.php
+++ application/views/dashboard/dashboardMenu.php
@@ -8,40 +8,39 @@
         <hr>
         <ul class="nav nav-stacked">
           <li class="btn-dashboard-menu" data-idlink="#dashboard-badge">
-            <a href="#" class="dashboard-menuitem">
+            <a href="#" class="dashboard-menuitem dashboard-active">
               Badge
-              <span class="pull-right"><button class="dashboard-button"><i class="glyphicon glyphicon-plus"></i></button></span>
+              <span class="pull-right"><button class="dashboard-button" data-form="#badge-form"><i class="glyphicon glyphicon-plus"></i></button></span>
             </a>
           </li>
           <li  class="btn-dashboard-menu" data-idlink="#dashboard-quest">
             <a href="#" class="dashboard-menuitem">
               Quest
-              <span class="pull-right"><button class="dashboard-button"><i class="glyphicon glyphicon-plus"></i></button></span>
+              <span class="pull-right"><button class="dashboard-button" data-form="#form-quest"><i class="glyphicon glyphicon-plus"></i></button></span>
             </a>
           </li>
           <li class="btn-dashboard-menu" data-idlink="#dashboard-party">
             <a href="#" class="dashboard-menuitem">
               Party
-              <span class="pull-right"><button class="dashboard-button"><i class="glyphicon glyphicon-plus"></i></button></span>
+              <span class="pull-right"><button class="dashboard-button" data-form="#party-form"><i class="glyphicon glyphicon-plus"></i></button></span>
             </a>
           </li>
           <li class="btn-dashboard-menu" data-idlink="#dashboard-office">
             <a href="#" class="dashboard-menuitem">
               Office
-              <span class="pull-right"><button class="dashboard-button"><i class="glyphicon glyphicon-plus"></i></button></span>
+              <span class="pull-right"><button class="dashboard-button" data-form="#office-form"><i class="glyphicon glyphicon-plus"></i></button></span>
             </a>
           </li>
           <?php if($isAdmin) { ?>
           <li class="btn-dashboard-menu" data-idlink="#dashboard-serial">
             <a href="#" class="dashboard-menuitem">
               Serial
-              <span class="pull-right"><button class="dashboard-button"><i class="glyphicon glyphicon-plus"></i></button></span>
             </a>
           </li>
           <li class="btn-dashboard-menu" data-idlink="#dashboard-noti">
             <a href="#" class="dashboard-menuitem">
               Notifications
-              <span class="pull-right"><button class="dashboard-button"><i class="glyphicon glyphicon-plus"></i></button></span>
+              <span class="pull-right label label-danger">0</span>
             </a>
           </li>
           <?php } ?>
