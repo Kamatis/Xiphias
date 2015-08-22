51e181e545c5ce5b266ba33e84e7474560f52012
Kamatis <rosales.matthew.allan@gmail.com>
2015-08-22 09:13:47 +0800

    added badgereward view

---
+++ application/views/dashboard/badgeRewards.php
@@ -0,0 +1,3 @@
+<div class="badge-reward-item " style="padding: 5px;" data-badgeid="<?php echo $badgeId; ?>">
+  <span><img src="<?php echo $imageSource; ?>" style="width: 50px; height: 50px;"></span> <?php echo $badgeName; ?>
+</div>
