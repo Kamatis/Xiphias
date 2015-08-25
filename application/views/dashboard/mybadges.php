e2880ba7c4d745f0366efcc7ea4eee3f59c2f56c
kellymilla18 <kellymilla18@gmail.com>
2015-08-25 22:11:37 +0800

    modified to show badge's name in the badge list

--- application/views/dashboard/mybadges.php
+++ application/views/dashboard/mybadges.php
@@ -1,3 +1,3 @@
 <li class="list-item-badge"  data-badgeid="<?php echo $badgeId; ?>">
-    <a href="#"><img class="icon" style="width:30px; height: 30px;" src="<?php echo $imageSource; ?>"> Badge Name</a>
+    <a href="#"><img class="icon" style="width:30px; height: 30px;" src="<?php echo $imageSource; ?>"> <?php echo $badgeName; ?></a>
 </li>
