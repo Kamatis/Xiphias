15f7fbf8b9666a33ca4c8e4e497d673eb5a270bd
Kamatis <rosales.matthew.allan@gmail.com>
2015-09-03 07:33:09 +0800

    removed carousel and put house rankings outside. removed comments. removed quest ads.

--- application/views/index.php
+++ application/views/index.php
@@ -1,6 +1,3 @@
-<!--credits-->
-<!--<a href='http://www.freepik.com/free-vector/variety-of-megaphones_789965.htm'>Designed by Freepik</a>-->
-
 <div class="container-fluid">
   <div class="col-sm-3 col-sm-push-9">
       <div id="feeder-container">
@@ -15,11 +12,6 @@
     </div>
   </div>
   <div class="col-sm-9 col-sm-pull-3">
-    <div >
-      <?php echo $carousel; ?>
-    </div>
-    <div class="" style="margin-top: 10px">
-      <img class="img-responsive" src="http://placehold.it/1200x450/16a085/ffffff?text=Quest Ads">
-    </div>
+    <div id="house-ranks-chart"></div>
   </div>
 </div>
