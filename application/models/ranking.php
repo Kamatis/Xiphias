9fcb06703f32fdf0eca058d2f3090114be25802e
kellymilla18 <kellymilla18@gmail.com>
2015-09-04 02:33:25 +0800

    added function to add rankings entry

--- application/models/ranking.php
+++ application/models/ranking.php
@@ -16,4 +16,13 @@ class Ranking extends CI_Model {
         });
         return $data;
     }
+    
+  public function addRankingsEntry($hof_id) {
+    $data['hof_id'] = $hof_id;
+    for($x = 1; $x <= 4; $x++) {
+       $data['house_id'] = $x;
+       $data['points'] = $this->house->getHousePoint($x);
+       $this->db->insert('ranking', $data);
+    }
+  }
 }
