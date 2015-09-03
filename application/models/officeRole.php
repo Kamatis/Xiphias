754ef09b509dbaa1e81823c294995c44d8d2deb0
kellymilla18 <kellymilla18@gmail.com>
2015-09-04 02:59:08 +0800

    added function to get members of an office

--- application/models/officeRole.php
+++ application/models/officeRole.php
@@ -4,4 +4,20 @@ class OfficeRole extends CI_Model {
   public function addRoleMember($data) {
     $this->db->insert('office_role', $data); 
   }
+  
+  public function getOfficeMembers($office_id) {
+    $this->db->where('office_id', $office_id);
+    $query = $this->db->get('office_role');
+    $x = 0;
+    foreach($query->result() as $member) {
+      $data[$x]['approved'] = ($member->approved == 1);
+      $data[$x]['username'] = $this->user->getUsername($member->user_id);
+      $data[$x]['role']     = $member->role;
+      $data[$x]['quest-actions'] = ($member->quest_permission == 1);
+      $data[$x]['badge-actions'] = ($member->badge_permission == 1);
+      $data[$x]['deleteRole'] = "1111";
+      $x++;
+    }
+    return $data;
+  }
 }
