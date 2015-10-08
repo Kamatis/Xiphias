0922c0ee6ea6142b6b28641bb630d1ec2c780087
francissalvamante <fsalvamante@gbox.adnu.edu.ph>
2015-10-08 19:32:44 +0800

    added the html2pdf library

---
+++ application/libraries/htmlpdf/Htmlpdf.php
@@ -0,0 +1,199 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( session_status() == PHP_SESSION_NONE ) {
+    session_start();
+}
+
+require_once(APPPATH . 'libraries/htmlpdf/html2pdf/html2pdf.class.php');
+
+class Htmlpdf {
+    var $ci;
+    public function __construct(){
+        $this->ci =& get_instance();
+    }
+    
+    public function convert(){
+        $width = 8.5 * 25.4; $height = 11 * 25.4;
+        $html = new HTML2PDF('P',array($width,$height),'en');
+        $content = '<table style="border-collapse; collapse; table-layout: fixed;" align="center">
+                    <tr><td style="width=380px; word-wrap: break-word; text-align: center; font-weight: bold; font-size: 24px;">Francis Vianney S. Salvamante</td></tr>
+                    <tr><td style="width=380px; word-wrap: break-word; text-align: center; font-size: 15px;">Address: Blk 19 Lot 2 Kapayapaan St. PHS II Del Rosario, Canaman, Camarines Sur</td></tr>
+                    <tr><td style="width=380px; word-wrap: break-word; text-align: center; font-size: 15px;">Contact number: +639151260088</td></tr>
+                    <tr><td style="width=380px; word-wrap: break-word; text-align: center; font-size: 15px;">Email: fsalvamante@gbox.adnu.edu.ph</td></tr>
+                    </table><hr>
+                    <p style="font-size: 13px; line-height: 0.5; font-weight: bold">Career Objective</p>
+                    <p style="font-size: 13px; line-height: 0.5; text-indent: 25px;">To be able to do database operations and optimize database queries for the company.</p>
+                    <p style="font-size: 13px; line-height: 0.5; font-weight: bold;">Educational Background</p>
+                    <table style="border-collapse: collapse; table-layout: fixed; width: 500px; margin-left: 15px;">
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center; font-weight: bold;">School Attended</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center; font-weight: bold;">Degree Attained</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center; font-weight: bold;">Inclusive Years</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Ateneo de Naga University</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Bachelor of Science in Information Technology</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center;">2012 - 2016</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Ateneo de Naga University - High School</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">High School Diploma</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center;">2008 - 2012</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Naga Central School I</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Elementary Diploma</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center;">2002 - 2008</td>
+                    </tr>
+                    </table> <br>
+                    <p style="font-size: 13px; line-height: 1 !important; font-weight: bold">Achievements and Accomplishments</p> <br>
+                    <table style="border-collapse: collapse; table-layout: fixed; width: 500px; margin-left: 15px;">
+                    <tr>
+                    <td style="border: solid 1px; width: 229px; word-wrap: break-word; text-align: center; font-weight: bold">School/Organization</td>
+                    <td style="border: solid 1px; width: 200px; word-wrap: break-word; text-align: center; font-weight: bold">Event</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center; font-weight: bold">Award</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center; font-weight: bold">Date</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 229px; word-wrap: break-word; text-align: center;">Association of Computing Machineries</td>
+                    <td style="border: solid 1px; width: 200px; word-wrap: break-word; text-align: center;">Inter-Collegiate Programming Competition (Nationals)</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">37th Place</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">February 26, 2013</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 229px; word-wrap: break-word; text-align: center;">Association of Computing Machineries</td>
+                    <td style="border: solid 1px; width: 200px; word-wrap: break-word; text-align: center;">Inter-Collegiate Programming Competition (Regionals)</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">3rd Place</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">September 6, 2014</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 229px; word-wrap: break-word; text-align: center;">Ateneo de Naga University</td>
+                    <td style="border: solid 1px; width: 200px; word-wrap: break-word; text-align: center;">Ateneo Programming Competition</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">5th Place</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">February 18, 2013</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 229px; word-wrap: break-word; text-align: center;">Ateneo de Naga University</td>
+                    <td style="border: solid 1px; width: 200px; word-wrap: break-word; text-align: center;">Ateneo Programming Competition</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">1st Place</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">February 22, 2014</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 229px; word-wrap: break-word; text-align: center;">Ateneo de Naga University</td>
+                    <td style="border: solid 1px; width: 200px; word-wrap: break-word; text-align: center;">Ateneo Programming Competition</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">1st Place</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">February 27, 2013</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 229px; word-wrap: break-word; text-align: center;">Ateneo de Naga University</td>
+                    <td style="border: solid 1px; width: 200px; word-wrap: break-word; text-align: center;">Ateneo Computer Programming Competition</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">2nd Place</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">July 23, 2014</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 229px; word-wrap: break-word; text-align: center;">Ateneo de Naga University</td>
+                    <td style="border: solid 1px; width: 200px; word-wrap: break-word; text-align: center;">Ateneo Computer Programming Competition</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">10th Place</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">August 11, 2015</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 229px; word-wrap: break-word; text-align: center;">University of the Philippines - Diliman</td>
+                    <td style="border: solid 1px; width: 200px; word-wrap: break-word; text-align: center;">UP Algolympics</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">1st Place</td>
+                    <td style="border: solid 1px; width: 150px; word-wrap: break-word; text-align: center;">February 21, 2015</td>
+                    </tr>
+                    </table>
+                    <p style="font-size: 13px; line-height: 0.5; font-weight: bold;">Trainings, Seminars and Conferences</p> <br>
+                    <table style="border-collapse: collapse; table-layout: fixed; width: 500px; margin-left: 15px;">
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center; font-weight: bold">Title</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center; font-weight: bold">Place</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center; font-weight: bold">Inclusive Dates</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Senior Practicum</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Nueva Caceres Technological Solutions</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center;">November 2015 - March 2016</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Google I/O Roadshow</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Ateneo de Naga University</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center;">August 26, 2015</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Seminar on Network Security</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">University of the Philippines - Diliman</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center;">October 12-14, 2012</td>
+                    </tr>
+                    </table>
+                    <p style="font-size: 13px; line-height: 0.5; font-weight: bold;">Skills and Interests</p>
+                    <ul style="list-style-type:circle; font-size: 13px;">
+                    <li>Can communicate orally and in writing</li>
+                    <li>Can play the guitar and the flute</li>
+                    <li>Computer literate</li>
+                    <li>Can Sing</li>
+                    <li>Can play basketball and badminton</li>
+                    </ul> <br>
+                    <page>
+                    <p style="font-size: 13px; line-height: 0.5; font-weight: bold;">Organizational Affiliations</p>
+                    <table style="border-collapse: collapse; table-layout: fixed; width: 500px; margin-left: 15px;">
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center; font-weight: bold;">Organization</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center; font-weight: bold;">Position</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center; font-weight: bold;">Inclusive Dates</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">The Ateneo Consortium for Technological Information and Computing Sciences</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">1st Year Representative</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center;">June 2012 - March 2013</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">The Ateneo Conortium for Technological Information and Computing Sciences</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Member</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center;">March 2013 - March 2016</td>
+                    </tr>
+                    <tr>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Ateneo Innovations Center</td>
+                    <td style="border: solid 1px; width: 250px; word-wrap: break-word; text-align: center;">Member</td>
+                    <td style="border: solid 1px; width: 235px; word-wrap: break-word; text-align: center;">October 2012 - March 2016</td>
+                    </tr>
+                    </table> <br>
+                    <p style="font-size: 13px; line-height: 0.5; font-weight: bold;">Personal Data</p>
+                    <p style="font-size: 13px; line-height: 0.5; text-indent: 25px;">Birthdate: September 14, 1995</p>
+                    <p style="font-size: 13px; line-height: 0.5; text-indent: 25px;">Civil Status: Single</p>
+                    <p style="font-size: 13px; line-height: 0.5; text-indent: 25px;">Nationality: Pilipino</p> <br>
+                    <table style="border-collapse: collapse; table-layout: fixed; width: 500px; margin-left: 25px;">
+                    <tr>
+                    <td style="width: 350px; word-wrap: break-word;">Mother\'s Name: Marymon G. Serrano-Salvamante</td>
+                    <td style="width: 250px; word-wrap: break-word;">Occupation: Birthdate: March 7, 1967</td>
+                    </tr>
+                    </table>
+                    <p style="font-size: 13px; line-height: 0.5; text-indent: 25px;">Occupation: Registrar - Ateneo de Naga University</p>
+                    <table style="border-collapse: collapse; table-layout: fixed; width: 500px; margin-left: 25px;">
+                    <tr>
+                    <td style="width: 350px; word-wrap: break-word;">Father\' Name: Larry C. Salvamante</td>
+                    <td style="width: 250px; word-wrap: break-word;">Occupation: Birthdate: April 21, 1965</td>
+                    </tr>
+                    </table>
+                    <p style="font-size: 13px; line-height: 0.5; text-indent: 25px;">Occupation: Employee - National Grid Corporation of the Philippines</p>
+                    <p style="font-size: 13px; line-height: 0.5; font-weight: bold;">Reference</p>
+                    <table style="margin-left: 25px;">
+                    <tr><td style="font-weight: bold;">Dr. Allan A. Sioson</td></tr>
+                    <tr><td>Dean, College of Computer Studies</td></tr>
+                    <tr><td>Ateneo de Naga University</td></tr>
+                    </table><br>
+                    <table style="margin-left: 25px;">
+                    <tr><td style="font-weight: bold;">Mr. Joshua C. Martinez</td></tr>
+                    <tr><td>Faculty, Department of Computer Science</td></tr>
+                    <tr><td>Ateneo de Naga University</td></tr>
+                    </table><br>
+                    <table style="margin-left: 25px;">
+                    <tr><td style="font-weight: bold;">Mr. Paul Angelo Silvestre</td></tr>
+                    <tr><td>Faculty, Department of Computer Science</td></tr>
+                    <tr><td>Ateneo de Naga University</td></tr>
+                    </table>
+                    </page>';
+        $html->writeHTML($content);
+        $html->Output('resume.pdf');
+    }
+}
